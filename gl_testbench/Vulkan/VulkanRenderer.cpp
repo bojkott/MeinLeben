719d42ac6c50a7d321e0139ab631f3f67f294440
morgan cromell <morgan.cromell@hotmail.com>
2018-01-23 11:51:40 +0100

    Added viewport, scissor and viewportState

--- gl_testbench/Vulkan/VulkanRenderer.cpp
+++ gl_testbench/Vulkan/VulkanRenderer.cpp
@@ -581,6 +581,22 @@ VkExtent2D VulkanRenderer::chooseSwapExtent(const VkSurfaceCapabilitiesKHR & cap
 
 void VulkanRenderer::createFixedFunctions()
 {
+	viewport.x = 0.0f;
+	viewport.y = 0.0f;
+	viewport.width = (float)swapChainExtent.width;
+	viewport.height = (float)swapChainExtent.height;
+	viewport.minDepth = 0.0f;
+	viewport.maxDepth = 1.0f;
+	
+	scissor.offset = { 0,0 };
+	scissor.extent = swapChainExtent;
+
+	VkPipelineViewportStateCreateInfo viewportState = {};
+	viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	viewportState.viewportCount = 1;
+	viewportState.pViewports = &viewport;
+	viewportState.scissorCount = 1;
+	viewportState.pScissors = &scissor;
 
 }
 
